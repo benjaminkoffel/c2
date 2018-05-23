a79f5dbb8eb47465bd8991e7e90e2c7e58cd5927
Benjamin Koffel <benjaminkoffel@gmail.com>
2018-05-23 16:04:12 +1000

    add proxy install.sh

---
+++ proxy/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+apt install apache2
+/etc/init.d/apache2 restart
+a2enmod rewrite proxy proxy_http ssl
+mkdir /etc/apache2/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/CN=local/O=local/C=US' -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+cat > /etc/apache2/sites-available/default-ssl.conf <<EOL
+<IfModule mod_ssl.c>
+    <VirtualHost *:443>
+        ServerAdmin admin@sec.myseek.xyz
+        ServerName proxy1.sec.myseek.xyz
+        ServerAlias proxy1.sec.myseek.xyz
+        DocumentRoot /var/www/html
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        SSLEngine on
+        SSLCertificateFile /etc/apache2/ssl/apache.crt
+        SSLCertificateKeyFile /etc/apache2/ssl/apache.key
+        <FilesMatch "\.(cgi|shtml|phtml|php)$">
+                        SSLOptions +StdEnvVars
+        </FilesMatch>
+        <Directory /usr/lib/cgi-bin>
+                        SSLOptions +StdEnvVars
+        </Directory>
+        BrowserMatch "MSIE [2-6]" \
+                       nokeepalive ssl-unclean-shutdown \
+                       downgrade-1.0 force-response-1.0
+        BrowserMatch "MSIE [17-9]" ssl-unclean-shutdown
+        SSLProxyEngine On
+        ProxyPass / https://c2.com/
+        ProxyPassReverse / https://c2.com/
+        SSLProxyCheckPeerCN off
+        SSLProxyCheckPeerName off
+        SSLProxyCheckPeerExpire off
+    </VirtualHost>
+</IfModule>
+EOL
+a2ensite default-ssl.conf
+/etc/init.d/apache2 restart
